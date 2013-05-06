0f6ecaf7d53b51bac3592d4594d50eca3d4db29d
Ben <benbakhshi@gmail.com>
2013-05-06 14:03:43 +0300

    commit

--- src/settings.py
+++ src/settings.py
@@ -117,11 +117,12 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sms',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'sms',
+
 )
 
 # A sample logging configuration. The only tangible logging
